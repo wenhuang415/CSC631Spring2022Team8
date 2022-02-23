786528d7d0e522b17fd104185933fe1cfabc84c0
Ashwinigm <ashwini.g.managuli@gmail.com>
2022-02-22 22:16:22 -0800

    Added Character Animation

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    [SerializeField] private float mouseSensitivity;
+
+    private Transform parent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        parent = transform.parent;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      Rotate();  
+    }
+
+    private void Rotate()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+
+        parent.Rotate(Vector3.up, mouseX);
+    }
+}
