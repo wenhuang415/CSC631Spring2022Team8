3b24ba9e1bc7cef8a0fc19d512de0e74d9dc82a3
Alex Hall <alexander.hall96@gmail.com>
2022-04-15 07:51:20 -0700

    Added multiple spawn areas and can spawn multiple at a time

--- Assets/Scripts/GameStateScript.cs
+++ Assets/Scripts/GameStateScript.cs
@@ -10,14 +10,17 @@ public class GameStateScript : MonoBehaviour
 
     public float shopTime;
 
+    public int EnemnyCount;
+
     public Object Enemy;
+    
 
     // Start is called before the first frame update
     void Start()
     {
         // gameState = 0;
         // shopTime = 30;//time in seconds
-        Debug.Log("Shop is open for " + shopTime + "seconds" );
+        Debug.Log("Shop is open for " + shopTime + " seconds" );
     }
 
     // Update is called once per frame
@@ -26,7 +29,7 @@ public class GameStateScript : MonoBehaviour
         if (gameState == 0)
         {
             shopTime = shopTime - Time.deltaTime;
-            Debug.Log("Shop is open for " + shopTime + "seconds" );
+            //Debug.Log("Shop is open for " + shopTime + "seconds" );
             if (shopTime <= 0)
             {
                 gameState = 1;
@@ -34,14 +37,38 @@ public class GameStateScript : MonoBehaviour
         }
         else if (gameState == 1)
         {
-            SpawnEnemies();
+            for(int i = 0;i <EnemnyCount; i++){
+                SpawnEnemies();
+            }
             gameState = 0;//just spawn 1 
-            shopTime = 10;
+            shopTime = 10;//reset shop time
         }
     }
 
     void SpawnEnemies()
     {
-        Instantiate(Enemy, new Vector3(36,1,13), Quaternion.identity);
+
+        int spawnLocation=Random.Range(0,3);//each int represents a different location
+        Vector3 vector = new Vector3(0,0,0);
+
+        switch(spawnLocation){
+            case 0:
+                 vector = new Vector3(Random.Range(38f,42f),1,Random.Range(11f,14f));
+                break;
+            case 1:
+                 vector = new Vector3(Random.Range(10f,14f),1,Random.Range(-6f,-9f));
+                break;
+            case 2:
+                 vector = new Vector3(Random.Range(10f,14f),1,Random.Range(25f,28f));
+                break;
+            case 3:
+                 vector = new Vector3(Random.Range(-8f,-5f),1,Random.Range(5f,9f));
+                break;
+            default:
+                break;
+
+        }
+        Debug.Log("Spawning at location " + spawnLocation);
+        Instantiate(Enemy, vector, Quaternion.identity);
     }
 }
