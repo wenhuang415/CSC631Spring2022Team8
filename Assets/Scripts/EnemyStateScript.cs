b84fc29c7a34c29824f5c8724d690ce312c747b9
Cody Huang <wenhuang@umail.ucsb.edu>
2022-05-01 18:46:09 -0700

    Added enemy HP bar

--- Assets/Scripts/EnemyStateScript.cs
+++ Assets/Scripts/EnemyStateScript.cs
@@ -1,24 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemyStateScript : MonoBehaviour
 {
     public float health;
-
-    
-
+    public float maxHealth;
+    public Slider slider;//hp ui
+    public GameObject healthBarUI;
     public object bullet;
     // Start is called before the first frame update
     void Start()
     {
-        health = 2;
+        health = maxHealth;
+        slider.value = CalculateHealth();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        slider.value = CalculateHealth();
+        if(health < maxHealth)
+        {
+            healthBarUI.SetActive(true);
+        }
+
+        if(health <= 0)
+        {
+            Destroy(gameObject);
+        }
     }
     void OnCollisionEnter(Collision collisionInfo)
     {
@@ -29,5 +40,10 @@ public class EnemyStateScript : MonoBehaviour
             //health-=collisionInfo.collider.damage;//need a reference to the damage values, maybe a different tag for each weapon? or a matrix with damage values and tags?
         }
 
+    }
+
+    float CalculateHealth()
+    {
+        return health / maxHealth;
     }
 }
