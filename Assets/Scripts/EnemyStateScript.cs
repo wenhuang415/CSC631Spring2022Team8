78bca9645e71f18aff8932a4ae558893d647ddf9
Cody Huang <wenhuang@umail.ucsb.edu>
2022-05-02 17:03:41 -0700

    added shooting script to gun

--- Assets/Scripts/EnemyStateScript.cs
+++ Assets/Scripts/EnemyStateScript.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEditor.Animations;
+
 public class EnemyStateScript : MonoBehaviour
 {
     public float health;
