a9dbb0d6682538c662a288395fc32dac5b271c0b
Ashwinigm <ashwinigm10@gmail.com>
2022-05-10 12:51:21 -0700

    GameOver menu

---
+++ Assets/GameOver.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour
+{
+
+    public void Setup()
+    {
+        gameObject.SetActive(true);
+    }
+   public void RestartButton()
+    {
+        Debug.Log("RESTART CLICKED");
+// SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+   public void QuitButton()
+    {
+        Debug.Log("QUIT CLICKED");
+// SceneManager.LoadScene("Menu");
+    }
+}
