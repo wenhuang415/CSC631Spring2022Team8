ec5bd52afafdaf36e89e6be6a6d8ca22849a4976
Ashwinigm <ashwinigm10@gmail.com>
2022-05-10 13:03:45 -0700

    Updated Gameover menu

--- Assets/GameOver.cs
+++ Assets/GameOver.cs
@@ -13,12 +13,12 @@ public class GameOver : MonoBehaviour
    public void RestartButton()
     {
         Debug.Log("RESTART CLICKED");
-// SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
    public void QuitButton()
     {
         Debug.Log("QUIT CLICKED");
-// SceneManager.LoadScene("Menu");
+        SceneManager.LoadScene("Menu");
     }
 }
